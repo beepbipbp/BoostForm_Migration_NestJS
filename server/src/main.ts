943e917bc5e5bf6af5a7d1280286dcc1af2de15e
beepbipbp <riskyjunk@gmail.com>
2023-02-18 00:11:23 +0900

    HttpExceptionFilter, ConfigModule 적용

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { AppModule } from "./app.module";
+import { HttpExceptionFilter } from "./common/filters/HttpException.filter";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  await app.listen(process.env.PORT);
 }
 bootstrap();
