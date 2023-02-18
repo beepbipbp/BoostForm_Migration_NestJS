8de4e6823855d9035d07af7a816915f5bfa5bbba
beepbipbp <riskyjunk@gmail.com>
2023-02-18 16:48:45 +0900

    MongooseModule 적용

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
+import { MongooseModule } from "@nestjs/mongoose";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 
@@ -8,6 +9,7 @@ import { AppService } from "./app.service";
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    MongooseModule.forRoot(process.env.MONGODB_URL),
   ],
   controllers: [AppController],
   providers: [AppService],
