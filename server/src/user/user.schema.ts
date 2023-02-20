2fffb33223b50c9ab82cf383598d12ba8c1cc1a5
beepbipbp <riskyjunk@gmail.com>
2023-02-20 17:07:56 +0900

    user schema 생성

---
+++ server/src/user/user.schema.ts
@@ -0,0 +1,22 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { IsNotEmpty, IsString } from "class-validator";
+import { Document } from "mongoose";
+
+@Schema()
+export class User extends Document {
+  @Prop({
+    type: String,
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @Prop({
+    type: String,
+  })
+  @IsString()
+  refresh_token: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
