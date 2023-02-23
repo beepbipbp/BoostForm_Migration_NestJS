78bff52b07116b3f48b758b21075c1519b368cdd
beepbipbp <riskyjunk@gmail.com>
2023-02-23 15:07:03 +0900

    form module 생성

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -6,6 +6,7 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { LoggerMiddleware } from "./common/middlewares/logger.middleware";
 import { UserModule } from "./user/user.module";
+import { FormModule } from './form/form.module';
 
 @Module({
   imports: [
@@ -20,6 +21,7 @@ import { UserModule } from "./user/user.module";
       },
     }),
     UserModule,
+    FormModule,
   ],
   controllers: [AppController],
   providers: [AppService],
